fe68a5e1e9c94dc978d95f1e741ff7a3938d166b
Ralph Lewis <rl3wis88@gmail.com>
2018-01-25 19:30:30 -0500

    Added some variables and a function

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// alerts are annoying, lets use console.log
+console.log('Welcome');
+/*This will now log to your console */
+
+
+
+// Creae a function that updates a DOM element
+function change(){
+  // the function body - this is where stuff will happen
+  document.getElementById('foo').innerHTML = 'Welcome';
+}
+
+// Basic data types in Javascript
+var name= 'Lucille'; //Store a string in a variable
+var numberOfWidgets = 10; //number
+var isCodingCool = true;// boolean
